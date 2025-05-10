66b38068c663111dfeb5d252c42bee34757a6ed6
uahjk <hjk70846@gmail.com>
2025-05-10 17:43:29 +0800

    wan cheng sort func

--- main.cpp
+++ main.cpp
@@ -1,13 +1,24 @@
-#includes <iostream>
+#include <iostream>
 using namespace std;
 
 void sort(int arr[], int size)
 {
+    for (int i = 0; i < size - 1; i++) {
+        // 每次遍历后，最大的元素会冒泡到最后
+        for (int j = 0; j < size - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // 交换相邻元素
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
 }
 
 int main()
 {
-	int arr[] = {12, 4, 89, 21, 78};
-	int size = sizeof arr / sizeof arr[0];
-	sort(arr, size);
+    int arr[] = {12, 4, 89, 21, 78};
+    int size = sizeof arr / sizeof arr[0];
+    sort(arr, size);
 }
